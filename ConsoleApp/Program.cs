15d5e2f09b9351b88a907770428da8b4bc8194ba
chris <chris@DESKTOP-9V2BK1M>
2025-05-31 14:20:44 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Entities;
+
+public class Program
+{
+
+    public static void Main(String[] args)
+    {
+        //INSTANCIA DEL GATO
+
+        var myCat = new Cat() { ID = 1, Name = "Chloe" };
+
+        Console.WriteLine("id del gato: " +  myCat.ID);
+        Console.WriteLine("Nombre del gato: " + myCat.Name);
+
+
+        Console.WriteLine("Ingrese una descripcion para el gato: ");
+
+        myCat.Description = Console.ReadLine();
+    }
+}
